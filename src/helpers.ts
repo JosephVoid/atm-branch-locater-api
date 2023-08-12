08cf61a0acee7a3e9778d37ada6fca34a8840db5
Yoseph Tenaw <yosephten@gmail.com>
2023-08-12 11:14:45 +0300

    Activated ESLint and turned of https

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,48 +1,66 @@
-import fs from "fs"
+import fs from "fs";
 import { RowDataPacket } from "mysql2";
 
-export interface IEntity extends RowDataPacket {ID: string, NAME: string, LATITIUDE: number, LONGITUDE: number, PIC: string, FID: string}
+export interface IEntity extends RowDataPacket {
+  ID: string;
+  NAME: string;
+  LATITIUDE: number;
+  LONGITUDE: number;
+  PIC: string;
+  FID: string;
+}
 
-export const getDist = (userLat:string, userLon:string, EntityList:any[]) => {
-    var EntityDist:{dist: number, ENTITY: IEntity}[] = [];
-    EntityList.forEach(ENT => {  
-      EntityDist.push(
-            {dist: calcCrow(Number(userLat), Number(userLon), ENT.LATITIUDE as number, ENT.LONGITUDE as number), 
-            ENTITY: ENT as IEntity}
-        );
+export const getDist = (
+  userLat: string,
+  userLon: string,
+  EntityList: any[]
+) => {
+  const EntityDist: { dist: number; ENTITY: IEntity }[] = [];
+  EntityList.forEach((ENT) => {
+    EntityDist.push({
+      dist: calcCrow(
+        Number(userLat),
+        Number(userLon),
+        ENT.LATITIUDE as number,
+        ENT.LONGITUDE as number
+      ),
+      ENTITY: ENT as IEntity,
     });
-    return EntityDist;
-}
-
-export const writeToLog = (dataToWrite:string) => {
-  fs.appendFile('log.txt', dataToWrite, function (err:any) {
-    if (err) throw err;
-    console.log('Saved!');
   });
-}
+  return EntityDist;
+};
+
+export const writeToLog = (dataToWrite: string) => {
+  fs.appendFile("log.txt", dataToWrite, function (err: any) {
+    if (err) throw err;
+    console.log("Saved!");
+  });
+};
 
 //This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in km)
-function calcCrow(lat1:number, lon1:number, lat2:number, lon2:number) 
-{
-  var R = 6371; // km
-  var dLat = toRad(lat2-lat1);
-  var dLon = toRad(lon2-lon1);
-  var lat1 = toRad(lat1);
-  var lat2 = toRad(lat2);
+function calcCrow(lat1: number, lon1: number, lat2: number, lon2: number) {
+  const R = 6371; // km
+  const dLat = toRad(lat2 - lat1);
+  const dLon = toRad(lon2 - lon1);
+  const lat1Rad = toRad(lat1);
+  const lat2Rad = toRad(lat2);
 
-  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  var d = R * c;
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.sin(dLon / 2) *
+      Math.sin(dLon / 2) *
+      Math.cos(lat1Rad) *
+      Math.cos(lat2Rad);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = R * c;
   return d;
 }
 
 // Converts numeric degrees to radians
-function toRad(Value:number) 
-{
-    return Value * Math.PI / 180;
+function toRad(Value: number) {
+  return (Value * Math.PI) / 180;
 }
 
-export function isNumeric(n:any) {
+export function isNumeric(n: any) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,13 @@
-import express from "express"
-import fileUpload from "express-fileupload"
-import dotenv from "dotenv"
-import mysql, { OkPacket, RowDataPacket } from "mysql2"
-import bodyParser from "body-parser"
-import https from "https"
-import bcrypt from "bcrypt"
-import fs from "fs"
-import {IEntity, getDist, isNumeric} from "./helpers"
-import { UploadedFile } from "express-fileupload"
-import { randomUUID } from "crypto"
-var privateKey  = fs.readFileSync('keys/privateKey.key', 'utf8');
-var certificate = fs.readFileSync('keys/certificate.crt', 'utf8');
-var credentials = {key: privateKey, cert: certificate};
+import express from "express";
+import fileUpload from "express-fileupload";
+import dotenv from "dotenv";
+import mysql, { RowDataPacket } from "mysql2";
+import bodyParser from "body-parser";
+import bcrypt from "bcrypt";
+import fs from "fs";
+import { getDist, isNumeric } from "./helpers";
+import { UploadedFile } from "express-fileupload";
+import { randomUUID } from "crypto";
 
 dotenv.config();
 
@@ -19,200 +15,257 @@ const app = express();
 
 // DB CONNECTION
 // --------------------------------------------------------------------
-var connection = mysql.createConnection({
-  host     : process.env.DB_HOST,
-  user     : process.env.DB_USER,
-  password : process.env.DB_PASS,
-  database : process.env.DB_NAME
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
-    console.error('Error Connecting: ' + err.stack);
+    console.error("Error Connecting: " + err.stack);
     return;
   }
 });
 
-
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(fileUpload());
 
 interface IUser extends RowDataPacket {
-  USERNAME: string,
-  PASSWORD: string
+  USERNAME: string;
+  PASSWORD: string;
 }
 
 // Authorize header
 app.use((req, res, next) => {
-  var authheader = req.headers.authorization;
-  if (!authheader) return res.status(401).json({message:"Authentication Needed"})
-  
-  var auth:any = Buffer.from(authheader.split(' ')[1], 'base64').toString().split(':');
-  var username = auth[0]; var pass = auth[1];
+  const authheader = req.headers.authorization;
+  if (!authheader)
+    return res.status(401).json({ message: "Authentication Needed" });
 
-  connection.query<IUser[]>('SELECT `PASSWORD` FROM users WHERE `USERNAME` = ?', [username], function (error, results, fields) {
-    if (error) res.status(500).send("error")
-    bcrypt.compare(pass, results[0].PASSWORD, function(err, result) {
-      if (result) next()
-      else return res.status(401).json("WRONG CREDS")
-    });
-  })
-})
+  const auth: any = Buffer.from(authheader.split(" ")[1], "base64")
+    .toString()
+    .split(":");
+  const username = auth[0];
+  const pass = auth[1];
 
-app.get("/allATM", (req,res, next) => {
-  connection.query('SELECT * FROM atmlocation', function (error, results, fields) {
-    if (error) res.status(500).send("error")
-    console.log(results)
-    res.send(results)
-  })
-})
-// /getPic?id=<>
-app.get("/getPic", (req,res, next) => {
-  var entityId = req.query.id
-  // Find in Cache
-  var fileList = fs.readdirSync(__dirname+'/upload_images')
-  for (var i = 0; i < fileList.length; i++){
-    var file = fileList[i]
-    if (file.includes(entityId as string))
-      return res.status(200).sendFile('./upload_images/'+entityId+'.jpg', { root: __dirname });
-  }
-  return res.status(400).json("NOT FOUND")
+  connection.query<IUser[]>(
+    "SELECT `PASSWORD` FROM users WHERE `USERNAME` = ?",
+    [username],
+    function (error, results, fields) {
+      if (error) res.status(500).send("error");
+      try {
+        bcrypt.compare(pass, results[0].PASSWORD, function (err, result) {
+          if (result) next();
+          else return res.status(401).json("WRONG CREDS");
+        });
+      } catch (error) {
+        return res.send("Please provide proper authorization");
+      }
+    }
+  );
 });
 
-app.get("/allBranch", (req,res, next) => {
-  connection.query('SELECT * FROM branchlocation', function (error, results, fields) {
-    if (error) res.status(500).send("error")
-    res.send(results)
-  })
-})
+app.get("/allATM", (req, res, next) => {
+  connection.query(
+    "SELECT * FROM atmlocation",
+    function (error, results, fields) {
+      if (error) res.status(500).send("error");
+      console.log(results);
+      res.send(results);
+    }
+  );
+});
+// /getPic?id=<>
+app.get("/getPic", (req, res, next) => {
+  const entityId = req.query.id;
+  // Find in Cache
+  const fileList = fs.readdirSync(__dirname + "/upload_images");
+  for (let i = 0; i < fileList.length; i++) {
+    const file = fileList[i];
+    if (file.includes(entityId as string))
+      return res
+        .status(200)
+        .sendFile("./upload_images/" + entityId + ".jpg", { root: __dirname });
+  }
+  return res.status(400).json("NOT FOUND");
+});
+
+app.get("/allBranch", (req, res, next) => {
+  connection.query(
+    "SELECT * FROM branchlocation",
+    function (error, results, fields) {
+      if (error) res.status(500).send("error");
+      res.send(results);
+    }
+  );
+});
 ///closeATM?number=<>&lat=<>&long=<>
-app.get("/closeATM", (req,res, next) => {
-  var lat = req.query.lat
-  var long = req.query.long
-  var num = req.query.number
-  
-  if (!isNumeric(lat) || !isNumeric(long) || !isNumeric(num)) return res.status(403).json("Bad Input")
-  connection.query('SELECT * FROM atmlocation', function (error, results, fields) {
-    if (error) res.status(500).send("error")
-    var ATM_DISTANCED = getDist(lat as string, long as string, results as any[]).sort((a, b) => a.dist - b.dist).slice(0,Number(num))
-    res.send(ATM_DISTANCED)
-  })
-})
+app.get("/closeATM", (req, res, next) => {
+  const lat = req.query.lat;
+  const long = req.query.long;
+  const num = req.query.number;
+
+  if (!isNumeric(lat) || !isNumeric(long) || !isNumeric(num))
+    return res.status(403).json("Bad Input");
+  connection.query(
+    "SELECT * FROM atmlocation",
+    function (error, results, fields) {
+      if (error) res.status(500).send("error");
+      const ATM_DISTANCED = getDist(
+        lat as string,
+        long as string,
+        results as any[]
+      )
+        .sort((a, b) => a.dist - b.dist)
+        .slice(0, Number(num));
+      res.send(ATM_DISTANCED);
+    }
+  );
+});
 ///closeBranch?number=<>&lat=<>&long=<>
-app.get("/closeBranch", (req,res, next) => {
-  var lat = req.query.lat
-  var long = req.query.long
-  var num = req.query.number
-  
-  if (!isNumeric(lat) || !isNumeric(long) || !isNumeric(num)) return res.status(403).json("Bad Input")
-  connection.query('SELECT * FROM branchlocation', function (error, results, fields) {
-    if (error) res.status(500).send("error")
-    var BRANCH_DISTANCED = getDist(lat as string, long as string, results as any[]).sort((a, b) => a.dist - b.dist).slice(0,Number(num))
-    res.send(BRANCH_DISTANCED)
-  })
-})
-///CRUD/:type/:entity?id=<>&lat=<>&lon=<>&name=<>
+app.get("/closeBranch", (req, res, next) => {
+  const lat = req.query.lat;
+  const long = req.query.long;
+  const num = req.query.number;
+
+  if (!isNumeric(lat) || !isNumeric(long) || !isNumeric(num))
+    return res.status(403).json("Bad Input");
+  connection.query(
+    "SELECT * FROM branchlocation",
+    function (error, results, fields) {
+      if (error) res.status(500).send("error");
+      const BRANCH_DISTANCED = getDist(
+        lat as string,
+        long as string,
+        results as any[]
+      )
+        .sort((a, b) => a.dist - b.dist)
+        .slice(0, Number(num));
+      res.send(BRANCH_DISTANCED);
+    }
+  );
+});
+///CRUD/:type/:entity?id=<>&lat=<>&lon=<>&name=<>&fid=<>&pic=<>
 app.post("/CRUD/:type/:entity", (req, res, next) => {
-  var entityId = req.query.id
-  var ent_lat = req.query.lat
-  var ent_long = req.query.lon
-  var ent_name = req.query.name
+  const entityId = req.query.id;
+  const ent_lat = req.query.lat;
+  const ent_long = req.query.lon;
+  const ent_name = req.query.name;
+  const ent_fid = req.query.fid;
+  const ent_pic = req.query.pic;
 
   switch (req.params.type) {
     case "DELETE":
       if (req.params.entity == "ATM") {
-        connection.query(`DELETE FROM atmlocation WHERE TERMINAL_ID = "${entityId}"`, function (error) {
-          if (error) throw error;
-        })
-        return res.status(200).json("OK")
-      }
-      else if (req.params.entity == "BRANCH") {
-        connection.query(`DELETE FROM branchlocation WHERE BRANCH_CODE = "${entityId}"`, function (error) {
-          if (error) throw error;
-        })
-        return res.status(200).json("OK")
+        connection.query(
+          `DELETE FROM atmlocation WHERE TERMINAL_ID = "${entityId}"`,
+          function (error) {
+            if (error) throw error;
+          }
+        );
+        return res.status(200).json("OK");
+      } else if (req.params.entity == "BRANCH") {
+        connection.query(
+          `DELETE FROM branchlocation WHERE BRANCH_CODE = "${entityId}"`,
+          function (error) {
+            if (error) throw error;
+          }
+        );
+        return res.status(200).json("OK");
       }
       break;
     case "CREATE":
       if (req.params.entity == "ATM") {
-        connection.query(`INSERT INTO atmlocation (ID,TERMINAL_ID, LOCATION, LATITIUDE, LONGITUDE) VALUES (?,?,?,?,?)`,
-        [ randomUUID(), entityId, ent_name, ent_lat, ent_long],
-        function (error) {
-          if (error) throw error;
-        })
-        return res.status(200).json("OK")
-      }
-      else if (req.params.entity == "BRANCH") {
-        connection.query(`INSERT INTO branchlocation (ID, BRANCH_CODE, LOCATION, LATITIUDE, LONGITUDE) VALUES (?,?,?,?,?)`,
-        [ randomUUID(), entityId, ent_name, ent_lat, ent_long],
-        function (error) {
-          if (error) throw error;
-          else return res.status(200).json("OK")
-        })
+        connection.query(
+          `INSERT INTO atmlocation (ID,TERMINAL_ID, LOCATION, LATITIUDE, LONGITUDE) VALUES (?,?,?,?,?)`,
+          [randomUUID(), entityId, ent_name, ent_lat, ent_long],
+          function (error) {
+            if (error) throw error;
+          }
+        );
+        return res.status(200).json("OK");
+      } else if (req.params.entity == "BRANCH") {
+        connection.query(
+          `INSERT INTO branchlocation (ID, BRANCH_CODE, LOCATION, LATITIUDE, LONGITUDE) VALUES (?,?,?,?,?)`,
+          [randomUUID(), entityId, ent_name, ent_lat, ent_long],
+          function (error) {
+            if (error) throw error;
+            else return res.status(200).json("OK");
+          }
+        );
       }
       break;
     case "UPDATE":
       if (req.params.entity == "ATM") {
-        connection.query(`UPDATE atmlocation SET LOCATION = COALESCE(?, LOCATION), LATITIUDE = COALESCE(?, LATITIUDE), LONGITUDE = COALESCE(?, LONGITUDE) WHERE TERMINAL_ID = ?`,
-        [ent_name, ent_lat, ent_long, entityId],
-        function (error) {
-          if (error) throw error;
-        })
-        return res.status(200).json("OK")
-      }
-      else if (req.params.entity == "BRANCH") {
-        connection.query(`UPDATE branchlocation SET LOCATION = COALESCE(?, LOCATION), LATITIUDE = COALESCE(?, LATITIUDE), LONGITUDE = COALESCE(?, LONGITUDE) WHERE BRANCH_CODE = ?`,
-        [ent_name, ent_lat, ent_long, entityId],
-        function (error) {
-          if (error) throw error;
-        })
-        return res.status(200).json("OK")
+        connection.query(
+          `UPDATE atmlocation SET LOCATION = COALESCE(?, LOCATION), LATITIUDE = COALESCE(?, LATITIUDE), LONGITUDE = COALESCE(?, LONGITUDE), FID = COALESCE(?, FID), PIC = COALESCE(?, PIC) WHERE TERMINAL_ID = ?`,
+          [ent_name, ent_lat, ent_long, ent_fid, ent_pic, entityId],
+          function (error) {
+            if (error) throw error;
+          }
+        );
+        return res.status(200).json("OK");
+      } else if (req.params.entity == "BRANCH") {
+        connection.query(
+          `UPDATE branchlocation SET LOCATION = COALESCE(?, LOCATION), LATITIUDE = COALESCE(?, LATITIUDE), LONGITUDE = COALESCE(?, LONGITUDE), FID = COALESCE(?, FID), PIC = COALESCE(?, PIC) WHERE BRANCH_CODE = ?`,
+          [ent_name, ent_lat, ent_long, ent_fid, ent_pic, entityId],
+          function (error) {
+            if (error) throw error;
+          }
+        );
+        return res.status(200).json("OK");
       }
       break;
     default:
       break;
   }
-})
+});
 
 // /upload?tid=<ATM>&enitity=<"ATM" | "BRANCH">
-// type="file" name="sampleFile" 
+// type="file" name="sampleFile"
 // method='post' encType="multipart/form-data">
 app.post("/upload", (req, res, next) => {
-  let inputF:UploadedFile | UploadedFile[]
-  let uploadPath;
-  let tid = req.query.tid;
-  let entity = req.query.entity;
+  const tid = req.query.tid;
+  const entity = req.query.entity;
 
   if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).send('No files were uploaded.');
+    return res.status(400).send("No files were uploaded.");
   }
   // The name of the input field (i.e. "imputF") is used to retrieve the uploaded file
-  inputF = req.files.sampleFile as UploadedFile;
-  uploadPath = __dirname + '/upload_images/'+tid+'.jpg';
+  const inputF: UploadedFile | UploadedFile[] = req.files
+    .sampleFile as UploadedFile;
+  const uploadPath = __dirname + "/upload_images/" + tid + ".jpg";
 
   // Use the mv() method to place the file somewhere on your server
-  inputF.mv(uploadPath, function(err:any) {
-    if (err)
-      return res.status(500).send("error");
-    
+  inputF.mv(uploadPath, function (err: any) {
+    if (err) return res.status(500).send("error");
+
     // Add pic to ATM db
     if (entity == "ATM") {
-      connection.query(`UPDATE atmlocation SET PIC="${tid+'.jpg'}",FID=NULL WHERE TERMINAL_ID = "${tid}"`, function (error) {
-        if (error) throw error;
-        return res.status(200).json("OK")
-      })
+      connection.query(
+        `UPDATE atmlocation SET PIC="${
+          tid + ".jpg"
+        }",FID=NULL WHERE TERMINAL_ID = "${tid}"`,
+        function (error) {
+          if (error) throw error;
+          return res.status(200).json("OK");
+        }
+      );
+    } else if (entity == "BRANCH") {
+      connection.query(
+        `UPDATE branchlocation SET PIC="${
+          tid + ".jpg"
+        }",FID=NULL WHERE BRANCH_CODE = "${tid}"`,
+        function (error) {
+          if (error) throw error;
+          return res.status(200).json("OK");
+        }
+      );
     }
-    else if (entity == "BRANCH") {
-      connection.query(`UPDATE branchlocation SET PIC="${tid+'.jpg'}",FID=NULL WHERE BRANCH_CODE = "${tid}"`, function (error) {
-        if (error) throw error;
-        return res.status(200).json("OK")
-      })
-    }
-  })
-})
+  });
+});
 
-var httpsServer = https.createServer(credentials, app);
-
-httpsServer.listen(443, () => {
-  console.log("Listening on 443!")
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
 });
